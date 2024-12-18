746e0fb394f7f30eb487bfff5dfdfbefe1dfc1ea
acechenry <108236840+acechenry@users.noreply.github.com>
2024-12-19 06:28:36 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   images: {
     domains: ['imgurl.chenry.eu.org'],
     unoptimized: true,
   },
+  api: {
+    bodyParser: {
+      sizeLimit: '50mb',
+    },
+    responseLimit: '50mb',
+  },
 }
 
-export default nextConfig
+export default nextConfig 
